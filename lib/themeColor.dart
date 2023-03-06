c06c5366bed80c0e00f2fe11bf3f24268cfca0a0
nutdaran <daran.thw@student.mahidol.edu>
2023-03-06 12:11:52 +0700

    add theme color

---
+++ lib/themeColor.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xffffde59);
+const blackColor = Color(0xFF1F1F1F);
+const unavailableColor = Color(0xffBDC2B9);
+const greyColor = Color(0xff848484);
+
