136d4dde047c6f503530c1e183ac9e84e4fef8d7
Pawara Somsongkul <pawara.sos@student.mahidol.edu>
2023-04-02 19:30:02 +0700

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/': (context) => HomeScreen(),
